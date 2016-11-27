8d30765a5e6144a7aba76ed82c0b306c5f06ce67
Subhas Dandapani <rdsubhas@gmail.com>
2016-11-27 18:41:20 +0100

    Add LICENSE

--- bin/cross-compile.sh
+++ bin/cross-compile.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
 # Generate license notices
-deps=$(go list -f '{{ join .Deps "\n"}}' . | grep -v "myke/")
+deps="myke $(go list -f '{{ join .Deps "\n"}}' . | grep -v myke)"
 rm -rf tmp
 mkdir -p tmp
 out="tmp/LICENSES"
-echo -e "LICENSES\n" > $out
+echo -e "OPEN SOURCE LICENSES\n" > $out
 
 for dep in $deps; do
 	if [ -d "$GOPATH/src/$dep" ]; then
 		notices=$(ls -d $GOPATH/src/$dep/* 2>/dev/null | grep -i -e "license" -e "licence" -e "copying" -e "notice")
-		echo -e "$dep\n\n" >> $out
-		for notice in $notices; do
-			cat $notice >> $out
-		done
-		echo -e "\n\n" >> $out
+		if [ ! -z "$notices" ]; then
+			echo -e "$dep\n\n" >> $out
+			for notice in $notices; do
+				cat $notice >> $out
+			done
+			echo -e "\n\n" >> $out
+		fi
 	fi
 done
 
