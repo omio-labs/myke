4960a814d564683840bcd49ad6e53683bc7984b3
Subhas Dandapani (RDX) <rdsubhas@users.noreply.github.com>
2017-04-25 23:04:18 +0200

    Add codecov integration with travis (#80)

--- bin/init.sh
+++ bin/init.sh
@@ -4,4 +4,5 @@ go get -u -v github.com/golang/lint/golint
 go get -u -v github.com/tools/godep
 go get -u -v github.com/mitchellh/gox
 go get -u -v github.com/jteeuwen/go-bindata/...
+go get -u -v github.com/go-playground/overalls
 godep restore -v
